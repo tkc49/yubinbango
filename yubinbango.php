4d01ab56bbf8f65faf7a96721c736f3f3c016ae2
tkc49 <h.tkc49@gmail.com>
2018-08-17 17:34:04 +0900

    first commit

---
+++ yubinbango.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name:     Yubinbango
+ * Plugin URI:
+ * Description:
+ * Author:          Takashi Hosoya
+ * Author URI:      https://takashihosoya.ninja/
+ * Text Domain:     yubinbango
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Yubinbango
+ */
+
+// Your code starts here.
+
+namespace yubinbango;
+
+if ( !class_exists('WPCF7')) {
+    return;
+}
+
+
+define( 'YUBINBANGO_URL',  plugins_url( '', __FILE__ ) );
+
+function load_js(){
+
+    wp_enqueue_script(
+        'yubinbango',
+        YUBINBANGO_URL . '/js/yubinbango.js',
+        array( )
+    );
+
+}
+add_action( 'wpcf7_load_js', '\yubinbango\load_js' );
+
+
+function form_class_attr($class){
+
+    return $class . " h-adr";
+
+}
+add_filter( 'wpcf7_form_class_attr', '\yubinbango\form_class_attr', 10 , 1 );
+
+
+function form_elements( $form ){
+
+
+    $form .= "\n" . '<span class="p-country-name" style="display:none;">Japan</span>' . "\n";
+    return $form;
+
+}
+
+add_filter( 'wpcf7_form_elements', '\yubinbango\form_elements', 10, 1);
